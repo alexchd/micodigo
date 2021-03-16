5a600829a217e69cbc317846e4db6973caed2f52
alexchd <alexchuqui@gmail.com>
2021-03-15 20:31:35 -0500

    practicas de javascript variables de tipo de texto y numero

---
+++ SEMANA04/CLASE17/variables tipo numero/codigo.js
@@ -0,0 +1,42 @@
+let nombre = "Josue";
+//variables de tipo numero
+let miEdad = 24;
+let miPeso = 70.5;
+//creo una variable para mi altura
+//creo una variable para mi peso
+//creo una variable que divida ni masa corpral la altura al cuadrado
+let altura = 1.7;
+//bmi=indice de masa corporal
+let bmi = miPeso / altura ** 2;
+alert(bmi);
+//hay 5 operaciones basicas
+/*
+la suma
+la multiplicacion
+diision
+la resta
+el modulo
+*/
+let a = 6;
+let b = 10;
+
+let suma = a + b;
+console.log(suma);
+
+let resta = b - a;
+console.log(resta);
+
+let multiplicacion = a * b;
+console.log(multiplicacion);
+
+let division = b / a;
+console.log(division);
+
+let potenciacion = a ** b;
+console.log(potenciacion);
+
+let radicacion = a ** 0.5;
+console.log(radicacion);
+
+let modulo = a % b;
+console.log(modulo);
---
+++ SEMANA04/CLASE17/variables/codigo.js
@@ -0,0 +1,15 @@
+
+
+
+// Este es un comentario de una linea
+
+/*
+Este es comentario multi lineas
+*/
+// variable de tipo texto
+let nombre= "Josue"
+let sistemaoperativo= "windows"
+console.log (typeof (nombre));
+alert (
+    `Mi segundo nombre es, $(nombre) y mi sistema operativo es, $(sistemaoperativo)`  
+)
