d54192b88c48b3a5627c17363b7fd28663c7a095
alexchd <alexchuqui@gmail.com>
2021-03-17 22:08:18 -0500

    ejercicios en javascript en array

---
+++ SEMANA04/CLASE03/ARREGLOS/codigo.js
@@ -0,0 +1,16 @@
+let arreglo =[1,2,3,4,5]
+
+let alumnos = ["pepito","maria","jose"]
+
+let mi_suerte= [true,false,true,true,false]
+let varios_elementos=["josue",24,"soletero",1.70,true]
+
+varios_elementos[0] //josue
+varios_elementos[1] //24
+varios_elementos[2] //"soletero"
+varios_elementos[3] //1.70
+varios_elementos[4] //true
+
+varios_elementos[100] 
+
+varios_elementos.length
